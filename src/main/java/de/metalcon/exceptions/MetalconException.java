06d4c3b04d3ad5af941607f19df99102936398a8
Jonas Kunze <jonas@metalcon.de>
2014-03-18 16:40:49 +0100

    first commit

---
+++ src/main/java/de/metalcon/exceptions/MetalconException.java
@@ -0,0 +1,9 @@
+package de.metalcon.exceptions;
+
+public class MetalconException extends Exception {
+	private static final long serialVersionUID = 4559694954542798171L;
+
+	public MetalconException(String message) {
+		super(message);
+	}
+}
